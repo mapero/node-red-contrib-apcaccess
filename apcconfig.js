7584f31cfb3bf658a263354ecd3517711cae5e42
Jochen Scheib <webmaster@mapero.de>
2017-05-07 13:36:11 +0200

    Reconnect functionality

--- apcconfig.js
+++ apcconfig.js
@@ -10,21 +10,29 @@ module.exports = function(RED) {
         node.name = n.name;
         node.host = n.host;
         node.port = n.port;
+        node.interval = 1000;
+        node.attemts = 0;
+        node.decay = 1.5;
+        node.maxInterval = 30 * 60 * 1000;
         node.client = new ApcAccess();
 
-        node.client.connect(node.host, node.port)
-          .then(function() {
-            node.log('Connected to '+node.host+':'+node.port);
-          }).catch( function(err) {
-            node.error(err);
-          });
+        function connect() {
+          node.client.connect(node.host, node.port);
+        }
 
         node.client.on('connect', () => {
+          node.attemts = 0;
           node.emit('status',{fill:"green",shape:"dot",text:"online"});
+          node.log('Connected to '+node.host+':'+node.port);
         });
 
         node.client.on('disconnect', () => {
+          node.attemts++;
+          var timeout = node.interval * Math.pow(node.decay, node.attemts);
+          timeout = timeout > node.maxInterval ? node.maxInterval : timeout;
           node.emit('status',{fill:"red",shape:"ring",text:"offline"});
+          node.log("Disconnected. Tries to reconnect in "+timeout/1000+" seconds.");
+          node.timeout = setTimeout(connect, timeout);
         });
 
         node.client.on('error', (error) => {
@@ -32,6 +40,9 @@ module.exports = function(RED) {
         });
 
         this.on("close", function() {
+          if(node.timeout) {
+            clearTimeout(node.timeout);
+          }
           if(node.client.isConnected) {
             node.client.disconnect()
               .then(function() {
@@ -40,7 +51,9 @@ module.exports = function(RED) {
                 node.error(err);
               });
           }
+          node.client.removeAllListeners();
         });
+        connect();
     }
 
     RED.nodes.registerType("apcconfig",ApcConfig);
